3a8a212b31138d63f4e5e54315289e305e1c8d90
stargazing20 <huder13168@gmail.com>
2021-04-17 12:27:03 -0500

    ejer 2

---
+++ ejer-2.js
@@ -0,0 +1,9 @@
+/*2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola". */
+
+const myFunction = (entry, n)=>{
+    word = String(entry);
+
+    console.log(word.substring(0,n))
+}
+
+myFunction("paragraph",4)
