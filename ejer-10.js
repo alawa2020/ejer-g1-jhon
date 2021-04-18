b79f7a8c33e6e0d35fb5fbc8c88f6b4151eff50f
stargazing20 <huder13168@gmail.com>
2021-04-18 06:30:42 -0500

    ejer 10

---
+++ ejer-10.js
@@ -0,0 +1,18 @@
+/* 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true. */
+
+const nCapicua = (n = undefined)=>{
+    return(n===undefined || n==="")
+     ?console.warn("enter a number")
+     :(n===0)
+      ?console.warn("the number cannot be 0")
+      :(Math.abs(n)===parseInt(String(Math.abs(n)).split("").reverse().join("")))
+        ?console.info("is capicua")
+        :console.info("isn't capicua")
+}
+
+nCapicua();
+nCapicua("")
+nCapicua(-4)
+nCapicua(-455)
+nCapicua(5654)
+nCapicua(565)
